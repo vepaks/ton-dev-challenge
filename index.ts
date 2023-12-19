adb4cdbd905153e9e8368c379935ade979607d4d
Aleksandar Kamburov <kamburoff@gmail.com>
2023-12-19 08:35:17 +0200

    initial local project

--- index.ts
+++ index.ts
@@ -1,2 +1,23 @@
 
-// write your NFT miner here
+import {Address, TonClient} from "ton"
+import {getHttpEndpoint} from "@orbs-network/ton-access";
+
+async function main () {
+
+    const wallet = Address.parse('UQBS5qr3a4ZliFjdzt_rLefDfLO6fF5oeQ7P1S-k4rRHj2qF');
+    const collection = Address.parse('EQDk8N7xM5D669LC2YACrseBJtDyFqwtSPCNhRWXU7kjEptX');
+
+    const endpoint = await getHttpEndpoint({
+        network: "testnet",
+    });
+
+    const client = new TonClient({ endpoint });
+
+    const miningData = await client.callGetMethod(collection, 'get_mining_data')
+
+    console.log(miningData)
+
+
+}
+
+main()
